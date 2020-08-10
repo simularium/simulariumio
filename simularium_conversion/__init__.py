765d6ffbe50712cafa68bbd783aa01b376fff87a
Blair Lyons <blair208@gmail.com>
2020-08-10 09:51:33 -0700

    cookie cutter

---
+++ simularium_conversion/__init__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+"""Top-level package for Simularium Conversion."""
+
+__author__ = "Blair Lyons"
+__email__ = "blairl@alleninstitute.org"
+# Do not edit this string manually, always use bumpversion
+# Details in CONTRIBUTING.md
+__version__ = "0.0.0"
+
+
+def get_module_version():
+    return __version__
+
+
+from .example import Example  # noqa: F401
