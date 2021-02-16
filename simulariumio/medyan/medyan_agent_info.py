c6ffef55d870bed62877770a4b21eec84b602eda
Blair Lyons <blair208@gmail.com>
2021-02-16 15:47:16 -0800

    option to draw endpoints for medyan objects and fixed scale factor

--- simulariumio/medyan/medyan_agent_info.py
+++ simulariumio/medyan/medyan_agent_info.py
@@ -13,8 +13,16 @@ log = logging.getLogger(__name__)
 class MedyanAgentInfo:
     name: str
     radius: float
+    draw_endpoints: bool
+    endpoint_radius: float
 
-    def __init__(self, name: str, radius: float = 1.0):
+    def __init__(
+        self,
+        name: str,
+        radius: float = 1.0,
+        draw_endpoints: bool = False,
+        endpoint_radius: float = 1.0,
+    ):
         """
         This object contains info about how to display a type of agent
 
@@ -28,6 +36,16 @@ class MedyanAgentInfo:
             A float radius for rendering this agent.
             this is the thickness of the line
             Default : 1.0
+        draw_points : bool
+            Draw spheres at the end points that define the object
+            in addition to a line connecting them?
+            Default: False
+        endpoint_radius : float
+            If drawing spheres at the end points that define the object,
+            what is their visualized radius?
+            Default: 1.0
         """
         self.name = name
         self.radius = radius
+        self.draw_endpoints = draw_endpoints
+        self.endpoint_radius = endpoint_radius
--- simulariumio/medyan/medyan_converter.py
+++ simulariumio/medyan/medyan_converter.py
@@ -35,7 +35,22 @@ class MedyanConverter(CustomConverter):
         """
         self._data = self._read(input_data)
 
-    def _parse_data_dimensions(self, lines: List[str]) -> List[int]:
+    def _draw_endpoints(
+        self, line: str, object_type: str, input_data: MedyanData
+    ) -> bool:
+        """
+        Parse a line of a MEDYAN snapshot.traj output file
+        and use MedyanAgentInfo for that agent type to
+        determine whether to also draw the endpoints as spheres
+        """
+        raw_tid = int(line.split()[2])
+        if raw_tid in input_data.agent_info[object_type]:
+            return input_data.agent_info[object_type][raw_tid].draw_endpoints
+        return False
+
+    def _parse_data_dimensions(
+        self, lines: List[str], input_data: MedyanData
+    ) -> List[int]:
         """
         Parse a MEDYAN snapshot.traj output file to get the number
         of subpoints per agent per timestep
@@ -43,6 +58,7 @@ class MedyanConverter(CustomConverter):
         totalSteps = 0
         max_agents = 0
         max_subpoints = 0
+        n = 0
         at_frame_start = True
         for line in lines:
             if len(line) < 1:
@@ -51,21 +67,33 @@ class MedyanConverter(CustomConverter):
             if at_frame_start:
                 # start of timestep
                 cols = line.split()
-                n = int(cols[2]) + int(cols[3]) + int(cols[4])
                 if n > max_agents:
                     max_agents = n
+                n = int(cols[2]) + int(cols[3]) + int(cols[4])
                 totalSteps += 1
                 at_frame_start = False
-            elif "FILAMENT" in line:
-                # start of filament
-                cols = line.split()
-                s = int(cols[3])
-                if max_subpoints < s:
-                    max_subpoints = s
-            elif "LINKER" in line or "MOTOR" in line:
-                # start of linker or motor
-                if max_subpoints < 2:
-                    max_subpoints = 2
+            elif "FILAMENT" in line or "LINKER" in line or "MOTOR" in line:
+                # start of object
+                if "FILAMENT" in line:
+                    object_type = "filament"
+                elif "LINKER" in line:
+                    object_type = "linker"
+                else:
+                    object_type = "motor"
+                if self._draw_endpoints(line, object_type, input_data):
+                    n += 2
+                if "FILAMENT" in line:
+                    # start of filament
+                    cols = line.split()
+                    s = int(cols[3])
+                    if max_subpoints < s:
+                        max_subpoints = s
+                else:
+                    # start of linker or motor
+                    if max_subpoints < 2:
+                        max_subpoints = 2
+        if n > max_agents:
+            max_agents = n
         return totalSteps, max_agents, max_subpoints
 
     def _get_trajectory_data(
@@ -76,7 +104,9 @@ class MedyanConverter(CustomConverter):
         """
         with open(input_data.path_to_snapshot, "r") as myfile:
             lines = myfile.read().split("\n")
-        totalSteps, max_agents, max_subpoints = self._parse_data_dimensions(lines)
+        totalSteps, max_agents, max_subpoints = self._parse_data_dimensions(
+            lines, input_data
+        )
         agent_data = AgentData(
             times=np.zeros(totalSteps),
             n_agents=np.zeros(totalSteps),
@@ -105,6 +135,8 @@ class MedyanConverter(CustomConverter):
             "motor": {},
         }
         last_tid = 0
+        object_type = ""
+        draw_endpoints = False
         for line in lines:
             if len(line) < 1:
                 at_frame_start = True
@@ -124,13 +156,14 @@ class MedyanConverter(CustomConverter):
                     object_type = "filament"
                 elif "LINKER" in line:
                     object_type = "linker"
-                elif "MOTOR" in line:
+                else:
                     object_type = "motor"
+                draw_endpoints = self._draw_endpoints(line, object_type, input_data)
                 # unique instance ID
                 raw_uid = int(cols[1])
                 if raw_uid not in uids[object_type]:
                     uids[object_type][raw_uid] = last_uid
-                    last_uid += 1
+                    last_uid += 1 if not draw_endpoints else 3
                 agent_data.unique_ids[t][n] = uids[object_type][raw_uid]
                 # type ID
                 raw_tid = int(cols[2])
@@ -139,7 +172,7 @@ class MedyanConverter(CustomConverter):
                     last_tid += 1
                 agent_data.type_ids[t][n] = tids[object_type][raw_tid]
                 # radius
-                agent_data.radii[t][n] = (
+                agent_data.radii[t][n] = input_data.scale_factor * (
                     input_data.agent_info[object_type][raw_tid].radius
                     if raw_tid in input_data.agent_info[object_type]
                     else 1.0
@@ -148,15 +181,37 @@ class MedyanConverter(CustomConverter):
                     agent_data.n_subpoints[t][n] = int(cols[3])
                 else:
                     agent_data.n_subpoints[t][n] = 2
+                # draw endpoints?
+                if draw_endpoints:
+                    for i in range(2):
+                        agent_data.viz_types[t][n + i + 1] = VIZ_TYPE.default
+                        agent_data.unique_ids[t][n + i + 1] = (
+                            uids[object_type][raw_uid] + i + 1
+                        )
+                        agent_data.type_ids[t][n + i + 1] = tids[object_type][raw_tid]
+                        agent_data.radii[t][n + i + 1] = input_data.scale_factor * (
+                            input_data.agent_info[object_type][raw_tid].endpoint_radius
+                            if raw_tid in input_data.agent_info[object_type]
+                            else 1.0
+                        )
                 parsing_object = True
             elif parsing_object:
                 # object coordinates
                 for i in range(len(cols)):
                     s = math.floor(i / 3)
                     d = i % 3
-                    agent_data.subpoints[t][n][s][d] = float(cols[i])
+                    agent_data.subpoints[t][n][s][d] = input_data.scale_factor * float(
+                        cols[i]
+                    )
+                    if draw_endpoints:
+                        agent_data.positions[t][n + s + 1][
+                            d
+                        ] = input_data.scale_factor * float(cols[i])
                 parsing_object = False
                 n += 1
+                if draw_endpoints:
+                    n += 2
+                    agent_data.n_agents[t] += 2
         return agent_data, tids
 
     def _read(self, input_data: MedyanData) -> Dict[str, Any]:
@@ -192,7 +247,7 @@ class MedyanConverter(CustomConverter):
                 else 0.0
             ),
             "totalSteps": totalSteps,
-            "spatialUnitFactorMeters": input_data.scale_factor * 1e-6,
+            "spatialUnitFactorMeters": 1e-9 / input_data.scale_factor,
             "size": {
                 "x": input_data.scale_factor * float(input_data.box_size[0]),
                 "y": input_data.scale_factor * float(input_data.box_size[1]),
