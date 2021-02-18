update trajectoryInfo after opening in FileConverter, fix test with units change

simulariumio/custom_converter.py
@@ -18,6 +18,7 @@ from .data_objects import (
     ScatterPlotData,
     AgentData,
     CustomData,
+    UnitData,
 )
 from .filters import (
     EveryNthAgentFilter,
@@ -375,13 +376,15 @@ class CustomConverter:
             agent_data = filter_class().filter_spatial_data(agent_data, params[i])
         self._data = self._read_custom_data(
             CustomData(
-                spatial_unit_factor_meters=self._data["trajectoryInfo"][
-                    "spatialUnitFactorMeters"
-                ],
                 box_size=np.array(
                     [float(box_size["x"]), float(box_size["y"]), float(box_size["z"])]
                 ),
                 agent_data=agent_data,
+                time_units=UnitData("s"),
+                spatial_units=UnitData(
+                    self._data["trajectoryInfo"]["spatialUnits"]["name"],
+                    self._data["trajectoryInfo"]["spatialUnits"]["magnitude"],
+                ),
             )
         )
         self._data["plotData"] = plot_data