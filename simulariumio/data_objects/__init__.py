d10803274c8e5d0b9ec029a69caf054a96588d0e
Blair Lyons <blair208@gmail.com>
2021-03-15 12:42:47 -0700

    added camera default specification to a test, fixed errors

--- simulariumio/data_objects/__init__.py
+++ simulariumio/data_objects/__init__.py
@@ -5,5 +5,6 @@ from .agent_data import AgentData  # noqa: F401
 from .trajectory_data import TrajectoryData  # noqa: F401
 from .meta_data import MetaData  # noqa: F401
 from .unit_data import UnitData  # noqa: F401
+from .camera_data import CameraData  # noqa: F401
 from .histogram_plot_data import HistogramPlotData  # noqa: F401
 from .scatter_plot_data import ScatterPlotData  # noqa: F401
