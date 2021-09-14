5eb47f635b60f687f6b3ebe14f3744ba6dce570d
Blair Lyons <blair208@gmail.com>
2021-09-14 10:48:27 -0700

    Squashed commit of the following:
    commit ce3fc81819e77cb024fa58ac46a654c97a039c2a
    Author: Blair Lyons <blair208@gmail.com>
    Date:   Thu Sep 9 13:25:18 2021 -0700
    
        Bump version: 1.1.0 → 1.1.1

--- setup.py
+++ setup.py
@@ -101,6 +101,6 @@ setup(
     url="https://github.com/allen-cell-animated/simulariumio",
     # Do not edit this string manually, always use bumpversion
     # Details in CONTRIBUTING.rst
-    version="1.1.0",
+    version="1.1.1",
     zip_safe=False,
 )
