2b9c4e4fc5d9f017088673005ed1f9587f9999cd
meganrm <meganrm@gmail.com>
2022-03-09 12:12:59 -0800

    add test

--- simulariumio/tests/converters/test_cellpack_converter.py
+++ simulariumio/tests/converters/test_cellpack_converter.py
@@ -57,12 +57,12 @@ def test_typeMapping(typeMapping, expected_typeMapping):
         (
             results["spatialData"]["bundleData"][0],
             [
-                1000.0, # default type
-                0.0, # id
-                0.0, # number of subpoints
-                25.0, # 750 shifted by the bounding box and scaled down by 0.1
+                1000.0,  # default type
+                0.0,  # id
+                0.0,  # number of subpoints
+                25.0,  # 750 shifted by the bounding box and scaled down by 0.1
                 25.0,
-                4.95, # 50 shifted by 0.5 and scaled down by 0.1
+                4.95,  # 50 shifted by 0.5 and scaled down by 0.1
                 1.1496245959205158,
                 0.17371416926080796,
                 1.4795316553717273,
@@ -74,3 +74,67 @@ def test_typeMapping(typeMapping, expected_typeMapping):
 )
 def test_bundleData(bundleData, expected_bundleData_data):
     assert expected_bundleData_data == bundleData["data"]
+
+
+@pytest.mark.parametrize(
+    "example_PDB, example_OBJ, example_FIBER",
+    [
+        (
+            {
+                "meshFile": "autoPACKserver/geometries/i1ysx_23kD_1.dae",
+                "coordsystem": "left",
+                "sphereFile": "autoPACKserver/collisionTrees/1ysx_23kD.sph",
+                "encapsulatingRadius": 14.59,
+                "name": "i1ysx_23kD",
+                "Type": "MultiSphere",
+                "meshName": "i1ysx_23kD",
+                "source": {"pdb": "1ysx", "transform": {"center": "true"}},
+                "use_mesh_rb": "false",
+                "pdb": "1ysx",
+            },
+            {
+                "color": [0.7843, 0.204, 0.204],
+                "sphereFile": "autoPACKserver/collisionTrees/Bacteria_Rad25_1_2.sph",
+                "encapsulatingRadius": 75,
+                "useOrientBias": "true",
+                "coordsystem": "left",
+                "meshFile": "autoPACKserver/geometries/Bacteria_Rad25_1_4.dae",
+                "meshName": "Bacteria_Rad25_1_3",
+                "name": "Bacteria_Rad25_1_3",
+                "Type": "MultiSphere",
+            },
+            {
+                "color": [1, 0.498, 0.314],
+                "coordsystem": "left",
+                "radii": [[25]],
+                "encapsulatingRadius": 75,
+                "positions2": [[[0, 150, 0]]],
+                "Type": "Grow",
+                "useLength": "false",
+                "uLength": 150,
+                "source": {"pdb": "1ATZ", "transform": {"center": "true"}},
+                "name": "snake",
+                "positions": [[[0, 0, 0]]],
+                "length": 8000,
+                "walkingMode": "sphere",
+            },
+        )
+    ],
+)
+def test_get_ingredient_display_data(example_PDB, example_OBJ, example_FIBER):
+    result_pdb = CellpackConverter._get_ingredient_display_data(
+        DISPLAY_TYPE.PDB, example_PDB, "url/"
+    )
+    result_obj = CellpackConverter._get_ingredient_display_data(
+        DISPLAY_TYPE.OBJ, example_OBJ, "url/"
+    )
+    result_fiber = CellpackConverter._get_ingredient_display_data(
+        DISPLAY_TYPE.FIBER, example_FIBER, "url/"
+    )
+    pdb_display_data = {"display_type": DISPLAY_TYPE.PDB, "url": "1ysx"}
+    obj_display_data = {"display_type": DISPLAY_TYPE.OBJ, "url": "url/Bacteria_Rad25_1_4.obj"}
+    fiber_display_data = {"display_type": DISPLAY_TYPE.FIBER, "url": "" }
+
+    assert result_pdb == pdb_display_data
+    assert result_obj == obj_display_data
+    assert result_fiber == fiber_display_data
