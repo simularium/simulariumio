8096849a1e558249f4ee3d7d06d7e3ca1c4438f0
Blair Lyons <blair208@gmail.com>
2022-07-25 15:56:07 -0700

    Bump version: 1.6.1 → 1.6.2

--- simulariumio/__init__.py
+++ simulariumio/__init__.py
@@ -6,7 +6,7 @@ __author__ = "Blair Lyons"
 __email__ = "blairl@alleninstitute.org"
 # Do not edit this string manually, always use bumpversion
 # Details in CONTRIBUTING.md
-__version__ = "1.6.1"
+__version__ = "1.6.2"
 
 
 def get_module_version():
