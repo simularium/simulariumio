3feeb49f9a7058c2ef51d157c77dc200710923f8
Blair Lyons <blair208@gmail.com>
2022-07-29 12:23:34 -0700

    add time index to output in readdy

--- simulariumio/readdy/readdy_converter.py
+++ simulariumio/readdy/readdy_converter.py
@@ -217,7 +217,7 @@ class ReaddyConverter(TrajectoryConverter):
                     ][particle_id].get_euler_angles()
                     type_name = result.types[time_index][agent_index]
                     if RotationUtility.vector_is_invalid(result.rotations[time_index][agent_index]):
-                        print(f"  {type_name} has invalid rotation")
+                        print(f"  {type_name} has invalid rotation at timestep # {time_index}")
         return result
 
     @staticmethod
