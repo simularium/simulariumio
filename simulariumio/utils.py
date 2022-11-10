262f4afb775d040bd2980e815825a7858b1057bf
ascibisz <alli.scibisz@alleninstitute.org>
2022-11-10 14:46:11 -0800

    only support xyz keys, not 012

--- simulariumio/utils.py
+++ simulariumio/utils.py
@@ -13,7 +13,7 @@ def unpack_position_vector(
     ----------
     vector_dict : Dict[str, str]
         Dict representing values of X, Y, and/or Z coordinates
-        Keys can either be {'0','1','2'} or {'x','y','z'}
+        Keys should be {'x','y','z'}
     defaults: np.ndarray
         Numpy array representing default XYZ coordinates. For any coordinates
         not provided in vector_dict, these default values will be used in the
@@ -23,22 +23,11 @@ def unpack_position_vector(
     if vector_dict is None:
         return defaults
 
-    # if the keys provided in vector_dict are {'x','y','z'}
-    if vector_dict.get("x") or vector_dict.get("y") or vector_dict.get("z"):
-        # use all positions given, but use defaults if any are missing
-        return np.array(
-            [
-                float(vector_dict.get("x", defaults[0])),
-                float(vector_dict.get("y", defaults[1])),
-                float(vector_dict.get("z", defaults[2])),
-            ]
-        )
-
-    # else, assume keys are {'0','1','2'}
+    # use all positions given, but use defaults if any are missing
     return np.array(
         [
-            float(vector_dict.get("0", defaults[0])),
-            float(vector_dict.get("1", defaults[1])),
-            float(vector_dict.get("2", defaults[2])),
+            float(vector_dict.get("x", defaults[0])),
+            float(vector_dict.get("y", defaults[1])),
+            float(vector_dict.get("z", defaults[2])),
         ]
     )
