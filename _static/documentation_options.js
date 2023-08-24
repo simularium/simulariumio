e8d8e8b92279020f34c9bd823ff38d4e0c4f6c64
mogres <saurabh.mogre@alleninstitute.org>
2023-08-24 00:10:15 +0000

    Deploying to gh-pages from @ simularium/simulariumio@983c99b0170d7f57b3dcf5825c78735fc889e20b 🚀

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,5 @@
-var DOCUMENTATION_OPTIONS = {
-    URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '1.7.2.dev37+gb2ab7fe',
+const DOCUMENTATION_OPTIONS = {
+    VERSION: '1.8.1.dev2+g983c99b',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
