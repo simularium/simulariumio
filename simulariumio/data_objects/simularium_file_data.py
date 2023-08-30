4a196d75a87d29b2735a81b3c94b09db62d079bb
ascibisz <alli.scibisz@alleninstitute.org>
2023-08-30 16:16:41 -0700

    lint fixes

--- simulariumio/data_objects/simularium_file_data.py
+++ simulariumio/data_objects/simularium_file_data.py
@@ -12,19 +12,16 @@ class SimulariumFileData:
         Return frame data for frame at index. If there is no frame at the index,
         return None.
         """
-        pass
 
     def get_index_for_time(self, time: float) -> int:
         """
         Return index for frame closest to a given timestamp
         """
-        pass
 
     def get_trajectory_info(self) -> Dict:
         """
         Return trajectory info block for trajectory, as dict
         """
-        pass
 
     def get_plot_data(self) -> Dict:
         pass
@@ -33,16 +30,13 @@ class SimulariumFileData:
         """
         Return the data of the trajectory, as a TrajectoryData object
         """
-        pass
 
     def get_file_contents(self) -> Union[Dict, bytes]:
         """
         Return raw file data, as dict for JSON file or as bytes for binary file
         """
-        pass
 
     def get_num_frames(self) -> int:
         """
         Return number of frames in the trajectory
         """
-        pass
