4eec7995dfc8dd8157c30cf1b897c36cb808a6d2
ascibisz <alli.scibisz@alleninstitute.org>
2023-10-30 11:03:05 -0700

    add abstract method decorators

--- simulariumio/data_objects/simularium_file_data.py
+++ simulariumio/data_objects/simularium_file_data.py
@@ -1,42 +1,56 @@
 from typing import Dict, Union
+from abc import ABC, abstractmethod
 from .trajectory_data import TrajectoryData
 from .frame_data import FrameData
 
 
-class SimulariumFileData:
+class SimulariumFileData(ABC):
     def __init__(self, file_contents: Union[str, bytes]):
         pass
 
+    @abstractmethod
     def get_frame_at_index(self, frame_number: int) -> Union[FrameData, None]:
         """
         Return frame data for frame at index. If there is no frame at the index,
         return None.
         """
+        pass
 
+    @abstractmethod
     def get_index_for_time(self, time: float) -> int:
         """
         Return index for frame closest to a given timestamp
         """
+        pass
 
+    @abstractmethod
     def get_trajectory_info(self) -> Dict:
         """
         Return trajectory info block for trajectory, as dict
         """
+        pass
 
+    @abstractmethod
     def get_plot_data(self) -> Dict:
         pass
 
+    @abstractmethod
     def get_trajectory_data_object(self) -> TrajectoryData:
         """
         Return the data of the trajectory, as a TrajectoryData object
         """
+        pass
 
+    @abstractmethod
     def get_file_contents(self) -> Union[Dict, bytes]:
         """
         Return raw file data, as dict for JSON file or as bytes for binary file
         """
+        pass
 
+    @abstractmethod
     def get_num_frames(self) -> int:
         """
         Return number of frames in the trajectory
         """
+        pass
