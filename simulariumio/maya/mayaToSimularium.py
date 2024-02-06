cc1270def3b10afe061c1d1dbb63fb27cde4706f
Blair Lyons <blair208@gmail.com>
2024-02-06 13:57:17 -0800

    script edits from Margot, ignore maya files

--- simulariumio/maya/mayaToSimularium.py
+++ simulariumio/maya/mayaToSimularium.py
@@ -21,33 +21,49 @@ INSTRUCTIONS:
 """   
 
 # edit these parameter values
-# geometry_urls = {
-#     "Antigen" : "https://www.dropbox.com/scl/fi/xh3vmyt9d74cl5cbhqgpm/Antigen.obj?rlkey=b4zcediso03wiuc96w3agm6rg&dl=1",
-#     "Antibody1" : "https://www.dropbox.com/scl/fi/1x8ei4xcn7apdi6l1h2gw/Antibody1.obj?rlkey=wak5oriwxuvupu71nayy2qsdf&dl=1",
-#     "Antibody2" : "https://www.dropbox.com/scl/fi/7ek5w2bltdkqai6aa27fy/Antibody2.obj?rlkey=g3mtgwgqh4c2xfc43jbvtg4l0&dl=1",
-# }
+
 geometry_urls = {
-    "Antigen" : "https://aics-simularium-data.s3.us-east-2.amazonaws.com/meshes/obj/Antigen.obj",
-    "Antibody1" : "https://aics-simularium-data.s3.us-east-2.amazonaws.com/meshes/obj/Antibody1.obj",
-    "Antibody2" : "https://aics-simularium-data.s3.us-east-2.amazonaws.com/meshes/obj/Antibody2.obj",
-    "gizmo" : "https://aics-simularium-data.s3.us-east-2.amazonaws.com/meshes/obj/maya_gizmo.obj",
-    "static" : "https://aics-simularium-data.s3.us-east-2.amazonaws.com/meshes/obj/Pi.obj",
-    "pTorus1" : "https://aics-simularium-data.s3.us-east-2.amazonaws.com/meshes/obj/TorusOBJ.obj",
-    "pCube1" : "https://aics-simularium-data.s3.us-east-2.amazonaws.com/meshes/obj/CubeOBJ.obj",
+     "Antigen1" : "https://www.dropbox.com/scl/fi/e77e7cyhx7kukrkk6fry8/Antigen.obj?rlkey=kpq7inxd4pkn0gs98f2c0quun&dl=0",
+     "Antigen2" : "https://www.dropbox.com/scl/fi/e77e7cyhx7kukrkk6fry8/Antigen.obj?rlkey=kpq7inxd4pkn0gs98f2c0quun&dl=0",
+     "Antigen3" : "https://www.dropbox.com/scl/fi/e77e7cyhx7kukrkk6fry8/Antigen.obj?rlkey=kpq7inxd4pkn0gs98f2c0quun&dl=0",
+     "Antigen4" : "https://www.dropbox.com/scl/fi/e77e7cyhx7kukrkk6fry8/Antigen.obj?rlkey=kpq7inxd4pkn0gs98f2c0quun&dl=0",
+     "Antigen5" : "https://www.dropbox.com/scl/fi/e77e7cyhx7kukrkk6fry8/Antigen.obj?rlkey=kpq7inxd4pkn0gs98f2c0quun&dl=0",
+     "Antigen6" : "https://www.dropbox.com/scl/fi/e77e7cyhx7kukrkk6fry8/Antigen.obj?rlkey=kpq7inxd4pkn0gs98f2c0quun&dl=0",
+     "Antigen7" : "https://www.dropbox.com/scl/fi/e77e7cyhx7kukrkk6fry8/Antigen.obj?rlkey=kpq7inxd4pkn0gs98f2c0quun&dl=0",
+     "Antigen8" : "https://www.dropbox.com/scl/fi/e77e7cyhx7kukrkk6fry8/Antigen.obj?rlkey=kpq7inxd4pkn0gs98f2c0quun&dl=0",
+     "Antigen9" : "https://www.dropbox.com/scl/fi/e77e7cyhx7kukrkk6fry8/Antigen.obj?rlkey=kpq7inxd4pkn0gs98f2c0quun&dl=0",
+     "Antigen10" : "https://www.dropbox.com/scl/fi/e77e7cyhx7kukrkk6fry8/Antigen.obj?rlkey=kpq7inxd4pkn0gs98f2c0quun&dl=0",
+     "Antigen11" : "https://www.dropbox.com/scl/fi/e77e7cyhx7kukrkk6fry8/Antigen.obj?rlkey=kpq7inxd4pkn0gs98f2c0quun&dl=0",
+     "Antibody1" : "https://www.dropbox.com/scl/fi/sp9nb0udohs3fe7nwzlrh/Antibody.obj?rlkey=t0jy5wdkx9039x6waeynjtn9c&dl=0",
+     "Antibody2" : "https://www.dropbox.com/scl/fi/sp9nb0udohs3fe7nwzlrh/Antibody.obj?rlkey=t0jy5wdkx9039x6waeynjtn9c&dl=0",
+     "Antibody3" : "https://www.dropbox.com/scl/fi/sp9nb0udohs3fe7nwzlrh/Antibody.obj?rlkey=t0jy5wdkx9039x6waeynjtn9c&dl=0",
+     "Antibody4" : "https://www.dropbox.com/scl/fi/sp9nb0udohs3fe7nwzlrh/Antibody.obj?rlkey=t0jy5wdkx9039x6waeynjtn9c&dl=0",
+     "Complex1": "https://www.dropbox.com/scl/fi/gaa670l67pi6bc4h0j9d9/Complex.obj?rlkey=d6vhexhgh1kibr39kzvxzp7s2&dl=0",
+     "Complex2": "https://www.dropbox.com/scl/fi/gaa670l67pi6bc4h0j9d9/Complex.obj?rlkey=d6vhexhgh1kibr39kzvxzp7s2&dl=0",
+     "Complex3": "https://www.dropbox.com/scl/fi/gaa670l67pi6bc4h0j9d9/Complex.obj?rlkey=d6vhexhgh1kibr39kzvxzp7s2&dl=0",
+     "Complex4": "https://www.dropbox.com/scl/fi/gaa670l67pi6bc4h0j9d9/Complex.obj?rlkey=d6vhexhgh1kibr39kzvxzp7s2&dl=0",
+     "Antigen" : "https://www.dropbox.com/scl/fi/e77e7cyhx7kukrkk6fry8/Antigen.obj?rlkey=kpq7inxd4pkn0gs98f2c0quun&dl=0",
+     "Antibody" : "https://www.dropbox.com/scl/fi/sp9nb0udohs3fe7nwzlrh/Antibody.obj?rlkey=t0jy5wdkx9039x6waeynjtn9c&dl=0",
+     "Complex": "https://www.dropbox.com/scl/fi/gaa670l67pi6bc4h0j9d9/Complex.obj?rlkey=d6vhexhgh1kibr39kzvxzp7s2&dl=0",
+     
 }
 spatial_units = "nm"  # nanometers
-timestep = 1.0  # time that passes each step
-time_units = "ns"  # nanoseconds
-output_path = "/Users/blairl/Dropbox/ForBlair_20231002/" # this file path must be absolute
+timestep = 2.0  # time that passes each step
+time_units = "us"  # microseconds
+
+# this file path must be absolute
+output_path = "/Users/blairl/Dropbox/ForBlair_20231002/"
+# output_path = "/Users/margotriggi/Documents/SpringSaladTutorial/TestAgAb/TestSimulariumExport"
+
 trajectory_name = "AgAb_animation" #.simularium
 display_title = "Antibody-Antigen animation"
 author_names = "Margot Riggi, Janet Iwasa"
 animation_description = "An animation of an antibody binding an antigen."
-box_dimensions = [100.0, 100.0, 100.0] # box will be centered at [0, 0, 0]
+box_dimensions = [300.0, 300.0, 300.0] # box will be centered at [0, 0, 0]
 camera_position=[0.0, 0.0, 200.0]
 camera_look_at_position=[0.0, 0.0, 0.0]
 camera_fov_degrees=60.0
-save_objs = True
+save_objs = False
 
 # don't edit below
 
@@ -76,17 +92,60 @@ positions = []
 rotations = []
 radii = []
 
+
 if max_agents < 1:
     raise Exception("Select one or more objects to export.")
 
+# TODO
+# to group agents by type (but couldn't find how to use it later)  
+def getClass(x):
+    word=(f"{x}")
+    if (word[4]=="g"):
+        return ("Antigen")
+    elif (word[4]=="b"):
+        return ("Antibody")
+    else:
+        return ("Complex")
+
+agent_types=set ()    
+type_names = cmds.ls(selection=True)
+for type_name in type_names:
+    agent_type=getClass(type_name)
+    agent_types.add(agent_type)
+
+
+# TODO
 # convert shader colors to hex codes
-def color_to_hex(raw_color):
+def num_to_letter(x):
+    if (x==10):
+         return ("a")
+    elif (x==11):
+         return ("b")
+    elif (x==12):
+         return ("c")
+    elif (x==13):
+         return ("d")
+    elif (x==14):
+         return ("e")
+    elif (x==15):
+         return ("f")
+    else:
+        return (x)
+        
+def color_to_hex(color):
     rgb_color = (
-        int(255 * raw_color[0][0]), 
-        int(255 * raw_color[0][1]), 
-        int(255 * raw_color[0][2])
+        int(255 * color[0][0]), 
+        int(255 * color[0][1]), 
+        int(255 * color[0][2])
     )
-    return '#%02x%02x%02x' % rgb_color
+    a=int((rgb_color[0])/16)
+    b=int((((rgb_color[0])/16)-a)*16)
+    c=int((rgb_color[1])/16)
+    d=int((((rgb_color[1])/16)-c)*16)
+    e=int((rgb_color[2])/16)
+    f=int((((rgb_color[2])/16)-e)*16)        
+    return ("#"+f"{num_to_letter(a)}{num_to_letter(b)}{num_to_letter(c)}{num_to_letter(d)}{num_to_letter(e)}{num_to_letter(f)}")
+
 
 def rotation_matrix_to_euler_angles(rotation_matrix):
     return -1 * Rotation.from_matrix(rotation_matrix).as_euler("xyz", degrees=False)
@@ -94,19 +153,36 @@ def rotation_matrix_to_euler_angles(rotation_matrix):
 # get display information
 display_data={}
 for type_name in type_names:
-    # get color from first shader
-    sel_shape = cmds.ls(objectsOnly=True, shapes=True)
-    shading_groups = cmds.listConnections(sel_shape, type='shadingEngine')
-    shaders = cmds.ls(cmds.listConnections(shading_groups), materials=True)
-    # TODO debug color
-    # color_hex = color_to_hex(cmds.getAttr(shaders[0]+".color"))
+#for agent_type in agent_types:
+    # get color 
+    shaders = cmds.listConnections(cmds.listHistory(type_name))
+    materials = [x for x in cmds.ls(cmds.listConnections(shaders), materials=1)]   
+    colorRGB= cmds.getAttr (f"{materials[0]}.color")
+    color_hex = color_to_hex(colorRGB)
     # create display data
     display_data[type_name] = DisplayData(
         name=type_name,
         display_type=DISPLAY_TYPE.OBJ,
         url=geometry_urls[type_name],
-        # color=color_hex,
+        color=color_hex,
     )
+    
+# get display information - one by agent type, but not sure how to use it later
+#display_data={}
+#for agent_type in agent_types:
+    # get color
+   # shaders = cmds.listConnections(cmds.listHistory(f"{agent_type}.1"))
+   # materials = [x for x in cmds.ls(cmds.listConnections(shaders), materials=1)]   
+   # colorRGB= cmds.getAttr (f"{materials[0]}.color")
+   # color_hex = color_to_hex(colorRGB)
+    # create display data
+   # display_data[agent_type] = DisplayData(
+        #name=agent_type,
+        #display_type=DISPLAY_TYPE.OBJ,
+        #url=geometry_urls[agent_type],
+       # color=color_hex,
+    #)
+
 
 # get trajectory
 for time_ix, time in enumerate(range(min_time, max_time + 1)):
@@ -115,6 +191,7 @@ for time_ix, time in enumerate(range(min_time, max_time + 1)):
     rotations.append([])
     radii.append([])
     for type_name in type_names:
+        vis=cmds.getAttr (f"{type_name}.v")
         position = cmds.xform(type_name, query=True, rotatePivot=True, worldSpace=True)
         transform = cmds.getAttr(f"{type_name}.worldMatrix", time=time)
         scale = [
@@ -127,12 +204,19 @@ for time_ix, time in enumerate(range(min_time, max_time + 1)):
             [transform[4] / scale[1], transform[5] / scale[1], transform[6] / scale[1]], 
             [transform[8] / scale[2], transform[9] / scale[2], transform[10] / scale[2]]
         ])
-        positions[time_ix].append(position)
-        rotations[time_ix].append(rotation_matrix_to_euler_angles(rotation_matrix))
-        radii[time_ix].append(scale[0])
+        # TODO
+        if vis==True:
+            positions[time_ix].append(position)
+            rotations[time_ix].append(rotation_matrix_to_euler_angles(rotation_matrix))
+            radii[time_ix].append(scale[0])
+            # might exist a more elegant way! I'm just sending the object far away when they are supposed to not be visible (can we just "skip" timepoints if objects are not visible in Maya?)
+        else:
+            positions[time_ix].append([2000,2000,2000])
+            rotations[time_ix].append(rotation_matrix_to_euler_angles(rotation_matrix))
+            radii[time_ix].append(scale[0])
 cmds.currentTime(min_time)
 
-# export OBJs
+# export OBJs TODO
 if save_objs:
     for type_name in type_names:
         cmds.select(type_name, replace=True)
